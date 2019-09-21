8f0d7ee966ca840db3cefcc64da2d1458f513390
zhuzhenhui <zhuzhenhui@ishunwan.com>
2019-09-21 13:28:17 +0800

    删除文件

--- client/src/views/components/Footer.vue
+++
@@ -1,6 +0,0 @@
-<template lang="pug">
-  <div>
-    <slot> &copy;2019 — master-5202m </slot>
-    <slot> &nbsp;&nbsp;&nbsp;&nbsp;Email —  158933822@qq.com </slot>
-  </div>
-</template>
--- client/src/views/public/401.vue
+++
@@ -1,3 +0,0 @@
-<template lang="pug">
-  <div>401权限页面</div>
-</template>
